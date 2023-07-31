fb97b2b179f0d1cc47c5c84343c92e321f3e3665
Sarahtonein <joemoe1899@gmail.com>
2023-07-31 21:08:38 +1000

    Updated HTML, and CSS

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function depositFunds(){
+//define / access instance for contract
 
+
+/* TODO:
+    hookup web3 wallet to the webpage and instance of contract
+    complete deposit functionality
+    have this working on a local EVM
+*/
+
+function depositFunds() {
+    // Your code for depositFunds function
 }
 
 function withDrawFunds() {
-
+    // Your code for withdrawFunds function
 }
 
 function plantSeed() {
+    // Your code for plantSeed function
+}
+
+function viewContractbalance() {
     
 }
+
+
+// Add event listener to the depositButton
+depositButton.addEventListener("click", depositFunds);
