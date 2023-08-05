886ba6ef809237376dc473520b772e7d434a1aee
Sarahtonein <joemoe1899@gmail.com>
2023-08-05 18:49:02 +1000

    Amended contract, index, and script

--- deploy.js
+++ deploy.js
@@ -3,9 +3,9 @@ const fs = require("fs-extra");
 
 async function main() {
     
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+    const provider = new ethers.providers.JsonRpcProvider("http://172.19.240.1:7545");
 
-    const wallet = new ethers.Wallet("0x424d3b78208f41b6685e776791b4a8db03585f21d7d7069dae492b0b7d6a0366", provider);
+    const wallet = new ethers.Wallet("0x2ba13968c3d853e9aa859dc82db8827f5da3f779b6dad4ffe4cbfd15183e0cb6", provider);
     const abi = fs.readFileSync("./Game_sol_Game.abi", "utf8");
     const binary = fs.readFileSync("./Game_sol_Game.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
--- script.js
+++ script.js
@@ -1,29 +1,112 @@
-/* TODO:
-    hookup web3 wallet to the webpage and instance of contract
-    complete deposit functionality
-    have this working on a local EVM
-*/
-const depositButton = document.getElementById("deposit")
-const withDrawButton = document.getElementById("withdraw");
-const plantSeedButton = document.getElementById("plantSeed");
+const Web3 = require('web3');
 
-depositButton.addEventListener("onclick", depositFunds);
-withDrawButton.addEventListener("onclick", withdrawFunds);
-plantSeedButton.addEventListener("onclick", plantSeed);
+// Replace 'YOUR_CONTRACT_ADDRESS' with the actual contract address
+const contractAddress = 'YOUR_CONTRACT_ADDRESS';
 
+// Replace with your contract ABI
+const contractABI = [
+  // Your contract ABI here
+];
 
-function depositFunds() {
-    // Your code for depositFunds function
+// Create a web3 instance using the user's provider
+async function initWeb3() {
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      // Request access to the user's MetaMask accounts
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      return new Web3(window.ethereum);
+    } catch (error) {
+      throw new Error('User denied account access');
+    }
+  } else if (typeof window.web3 !== 'undefined') {
+    // Legacy dapp browsers
+    return new Web3(window.web3.currentProvider);
+  } else {
+    throw new Error('Please install MetaMask to use this dApp.');
+  }
 }
 
-function withdrawFunds() {
-    // Your code for withdrawFunds function
+// Function to get the user's address
+async function getUserAddress(web3) {
+  const accounts = await web3.eth.getAccounts();
+  return accounts[0];
 }
 
-function plantSeed() {
-    // Your code for plantSeed function
+// Function to deposit funds to the smart contract
+async function depositFunds(web3, userAddress, value) {
+  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+  try {
+    await contractInstance.methods.depositFunds().send({ from: userAddress, value: web3.utils.toWei(value.toString(), 'ether') });
+    console.log('Deposit successful');
+  } catch (error) {
+    console.error('Error depositing funds:', error);
+  }
 }
 
-function viewContractbalance() {
-    
+// Function to withdraw funds from the smart contract
+async function withdrawFunds(web3, userAddress, withdrawalAmount) {
+  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+  try {
+    await contractInstance.methods.withdrawDeposit(withdrawalAmount).send({ from: userAddress });
+    console.log('Withdrawal successful');
+  } catch (error) {
+    console.error('Error withdrawing funds:', error);
+  }
 }
+
+// Function to check the user's balance in the smart contract
+async function checkBalance(web3, userAddress) {
+  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+  try {
+    const balance = await contractInstance.methods.checkBalance().call({ from: userAddress });
+    console.log('User balance:', balance);
+  } catch (error) {
+    console.error('Error checking balance:', error);
+  }
+}
+
+// Function to play a game in the smart contract
+async function playGame(web3, userAddress) {
+  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+
+  try {
+    const result = await contractInstance.methods.playAGame().call({ from: userAddress });
+    console.log('Game result:', result);
+  } catch (error) {
+    console.error('Error playing the game:', error);
+  }
+}
+
+// Main function to interact with the smart contract
+async function main() {
+  const web3 = await initWeb3();
+  const userAddress = await getUserAddress(web3);
+
+  // Get the button elements
+  const depositButton = document.getElementById('deposit');
+  const withdrawButton = document.getElementById('withdraw');
+  const playAGameButton = document.getElementById('playAGame');
+  const checkBalanceButton = document.getElementById('checkBalance');
+
+  // Add event listeners to the buttons
+  depositButton.addEventListener('click', async () => {
+    await handleDepositButtonClick(web3, userAddress, 0.1); // Example: Deposit 0.1 Ether
+  });
+
+  withdrawButton.addEventListener('click', async () => {
+    await handleWithdrawButtonClick(web3, userAddress, 0.05); // Example: Withdraw 0.05 Ether
+  });
+
+  playAGameButton.addEventListener('click', async () => {
+    await handlePlayAGameButtonClick(web3, userAddress);
+  });
+
+  checkBalanceButton.addEventListener('click', async () => {
+    await handleCheckBalanceButtonClick(web3, userAddress);
+  });
+}
+
+main();
