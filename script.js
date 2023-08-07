e31998313e28c8afd4eab766b1c825a79802c691
Sarahtonein <109805833+Sarahtonein@users.noreply.github.com>
2023-08-07 20:00:27 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,319 @@
-const Web3 = require('web3');
-
-// Replace 'YOUR_CONTRACT_ADDRESS' with the actual contract address
-const contractAddress = 'YOUR_CONTRACT_ADDRESS';
-
-// Replace with your contract ABI
+const contractAddress = '0xC01BE1207B2477064F3d4BF2dDaed0d687e7FD5f';
 const contractABI = [
-  // Your contract ABI here
+	{
+		"inputs": [],
+		"name": "depositFunds",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_betAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "playAGame",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_withdrawalAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdrawFunds",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "checkBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getRandomNumber",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
-
-// Create a web3 instance using the user's provider
-async function initWeb3() {
-  if (typeof window.ethereum !== 'undefined') {
+let web3;
+let contractInstance;
+// Function to connect to MetaMask
+async function connectToMetaMask() {
     try {
-      // Request access to the user's MetaMask accounts
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      return new Web3(window.ethereum);
+        if (window.ethereum) {
+            web3 = new Web3(window.ethereum);
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            alert('Connected to MetaMask!');
+            
+            // Initialize the contract instance here after connecting to MetaMask
+            const contract = new web3.eth.Contract(contractABI, contractAddress);
+            contractInstance = contract;
+        } else {
+            alert('MetaMask not detected. Please install MetaMask extension.');
+        }
     } catch (error) {
-      throw new Error('User denied account access');
+        alert('Failed to connect to MetaMask. Please try again.');
     }
-  } else if (typeof window.web3 !== 'undefined') {
-    // Legacy dapp browsers
-    return new Web3(window.web3.currentProvider);
-  } else {
-    throw new Error('Please install MetaMask to use this dApp.');
-  }
 }
-
-// Function to get the user's address
-async function getUserAddress(web3) {
-  const accounts = await web3.eth.getAccounts();
-  return accounts[0];
+// Function to disconnect from MetaMask
+async function disconnectFromMetaMask() {
+    try {
+        if (web3 && web3.currentProvider && web3.currentProvider.disconnect) {
+            await web3.currentProvider.disconnect();
+            web3 = undefined; // Clear the web3 instance
+            contractInstance = undefined; // Clear the contract instance
+            alert('Disconnected from MetaMask!');
+        } else {
+            alert('MetaMask not detected or not connected.');
+        }
+    } catch (error) {
+        alert('Failed to disconnect from MetaMask. Please try again.');
+    }
 }
+async function withdrawDeposit() {
+    try {
+        if (!web3 || !contractInstance) {
+            alert('Please connect to MetaMask first.');
+            return;
+        }
 
-// Function to deposit funds to the smart contract
-async function depositFunds(web3, userAddress, value) {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+        const withdrawalInput = document.getElementById('withdrawAmount');
+        const withdrawalValue = withdrawalInput.value;
 
-  try {
-    await contractInstance.methods.depositFunds().send({ from: userAddress, value: web3.utils.toWei(value.toString(), 'ether') });
-    console.log('Deposit successful');
-  } catch (error) {
-    console.error('Error depositing funds:', error);
-  }
+        // Validate the withdrawal value
+        if (isNaN(withdrawalValue) || withdrawalValue <= 0) {
+            alert('Invalid withdrawal amount. Please enter a positive number.');
+            return;
+        }
+
+        // Convert withdrawal amount to wei
+        const amountToWithdraw = web3.utils.toWei(withdrawalValue.toString(), 'ether');
+
+        // Call the withdrawal function on the contract
+        const accounts = await web3.eth.getAccounts();
+        const transaction = await contractInstance.methods.withdrawFunds(amountToWithdraw).send({
+            from: accounts[0],
+            gas: 200000 // Adjust the gas limit as needed
+        });
+
+        console.log('Transaction hash:', transaction.transactionHash);
+    } catch (error) {
+        console.error('Error while withdrawing funds:', error);
+        alert('Error while withdrawing funds: ' + error.message); // Provide specific error message
+    }
 }
+async function playAGame() {
+    try {
+        if (!web3 || !contractInstance) {
+            alert('Please connect to MetaMask first.');
+            return;
+        }
 
-// Function to withdraw funds from the smart contract
-async function withdrawFunds(web3, userAddress, withdrawalAmount) {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+        const betAmountInput = document.getElementById('gambleAmount');
+        const betAmountValue = betAmountInput.value;
 
-  try {
-    await contractInstance.methods.withdrawDeposit(withdrawalAmount).send({ from: userAddress });
-    console.log('Withdrawal successful');
-  } catch (error) {
-    console.error('Error withdrawing funds:', error);
-  }
+        // Validate the bet amount
+        if (isNaN(betAmountValue) || betAmountValue <= 0) {
+            alert('Invalid bet amount. Please enter a positive number.');
+            return;
+        } else {
+			if(betAmountValue > 0.025) {
+				alert('Invalid bet amount, max is 0.025');
+				return;
+			}
+		}
+
+        // Convert bet amount to wei
+        const betAmount = web3.utils.toWei(betAmountValue.toString(), 'ether');
+
+        // Call the playAGame function on the contract with the user input
+        const accounts = await web3.eth.getAccounts();
+		const transaction = await contractInstance.methods.playAGame(betAmount).send({
+			from: accounts[0],
+			gas: 200000 // Adjust the gas limit as needed
+		});
+		
+        console.log('Transaction hash:', transaction.transactionHash);
+    } catch (error) {
+        console.error('Error while playing the game:', error);
+        alert('Error while playing the game: ' + error.message); // Provide specific error message
+    }
 }
+async function depositFunds() {
+    try {
+        if (!web3 || !contractInstance) {
+            alert('Please connect to MetaMask first.');
+            return;
+        }
 
-// Function to check the user's balance in the smart contract
-async function checkBalance(web3, userAddress) {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+        const depositInput = document.getElementById('depositAmount');
+        const depositValue = depositInput.value;
 
-  try {
-    const balance = await contractInstance.methods.checkBalance().call({ from: userAddress });
-    console.log('User balance:', balance);
-  } catch (error) {
-    console.error('Error checking balance:', error);
-  }
+        // Validate the deposit value
+        if (isNaN(depositValue) || depositValue <= 0) {
+            alert('Invalid deposit amount. Please enter a valid positive number.');
+            return;
+        }
+
+        // Convert the deposit amount to wei
+        const amountToDeposit = web3.utils.toWei(depositValue, 'ether');
+
+        // Call the deposit function on the contract
+        const accounts = await web3.eth.getAccounts();
+        const transaction = await contractInstance.methods.depositFunds().send({
+            from: accounts[0],
+            value: amountToDeposit,
+            gas: 200000 // Adjust the gas limit as needed
+        });
+
+        console.log('Transaction hash:', transaction.transactionHash);
+    } catch (error) {
+        console.error('Error while depositing funds:', error);
+    }
 }
+// The checkBalance function modified to display the result in the HTML div
+async function checkBalance() {
+    try {
+        if (!web3 || !contractInstance) {
+            alert('Please connect to MetaMask first.');
+            return;
+        }
 
-// Function to play a game in the smart contract
-async function playGame(web3, userAddress) {
-  const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+        const userAddress = await getCurrentUserAddress();
+        const balance = await contractInstance.methods.checkBalance().call({ from: userAddress });
+		const balanceInEther = web3.utils.fromWei(balance, 'ether')
+        console.log('User contract balance:', balanceInEther);
 
-  try {
-    const result = await contractInstance.methods.playAGame().call({ from: userAddress });
-    console.log('Game result:', result);
-  } catch (error) {
-    console.error('Error playing the game:', error);
-  }
+        // Display the balance in the HTML div
+        const balanceContainer = document.getElementById('balanceContainer');
+        balanceContainer.textContent = `Your contract balance: ${balanceInEther} ETH`;
+		balanceContainer.style.color = "green";
+    } catch (error) {
+        console.error('Error while retrieving balance:', error);
+    }
 }
-
-// Main function to interact with the smart contract
-async function main() {
-  const web3 = await initWeb3();
-  const userAddress = await getUserAddress(web3);
-
-  // Get the button elements
-  const depositButton = document.getElementById('deposit');
-  const withdrawButton = document.getElementById('withdraw');
-  const playAGameButton = document.getElementById('playAGame');
-  const checkBalanceButton = document.getElementById('checkBalance');
-
-  // Add event listeners to the buttons
-  depositButton.addEventListener('click', async () => {
-    await handleDepositButtonClick(web3, userAddress, 0.1); // Example: Deposit 0.1 Ether
-  });
-
-  withdrawButton.addEventListener('click', async () => {
-    await handleWithdrawButtonClick(web3, userAddress, 0.05); // Example: Withdraw 0.05 Ether
-  });
-
-  playAGameButton.addEventListener('click', async () => {
-    await handlePlayAGameButtonClick(web3, userAddress);
-  });
-
-  checkBalanceButton.addEventListener('click', async () => {
-    await handleCheckBalanceButtonClick(web3, userAddress);
-  });
+// Helper function to get the current user's address from MetaMask
+async function getCurrentUserAddress() {
+    try {
+        // Get the selected account address
+        const accounts = await web3.eth.getAccounts();
+        return accounts[0];
+    } catch (error) {
+        console.error('Error while getting user address:', error);
+    }
 }
+// Adding event listeners to the buttons
+document.addEventListener('DOMContentLoaded', function () {
+    const connectButton = document.getElementById('connect');
+    const disconnectButton = document.getElementById('disconnect');
+    const depositButton = document.getElementById('deposit');
+	const withdrawButton = document.getElementById('withdraw');
+	const displayBalanceButton = document.getElementById('checkBalance');
+	const playGameButton = document.getElementById('playAGame');
+    
+	if (playGameButton) {
+        playGameButton.addEventListener('click', playAGame);
+    }
+    if (connectButton) {
+        connectButton.addEventListener('click', connectToMetaMask);
+    }
 
-main();
+    if (disconnectButton) {
+        disconnectButton.addEventListener('click', disconnectFromMetaMask);
+    }
+
+    if (depositButton) {
+        depositButton.addEventListener('click', depositFunds);
+    }
+
+	if (withdrawButton) {
+		withdrawButton.addEventListener('click', withdrawDeposit);
+	}
+
+	if (displayBalanceButton) {
+		displayBalanceButton.addEventListener('click', checkBalance);
+	}
+});
